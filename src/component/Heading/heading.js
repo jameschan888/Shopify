e8b02f9e0a6587d24610de632636a2e17f77df70
James <james@Jamess-iMac.local>
2018-07-06 01:49:45 +0400

    remove unused components.

--- src/component/Heading/heading.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./heading.module.css";
-const heading = props => (
-  <h1 className={props.isblack ? classes.black : classes.white}>
-    {props.text}
-  </h1>
-);
-export default heading;
